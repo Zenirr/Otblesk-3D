260948743e711e10fbd49fce249ba7fd0252f4f8
Zenirr <zenirr@mail.ru>
2024-05-02 19:40:40 +0600

    DotWeen Setted

--- Assets/Scripts/CameraScripts/MainMenuCameraMover.cs
+++ Assets/Scripts/CameraScripts/MainMenuCameraMover.cs
@@ -10,12 +10,15 @@ public class MainMenuCameraMover : MonoBehaviour
     [SerializeField] private Camera m_Camera;
     [SerializeField] private Vector3 _endPosition;
     [SerializeField] private float _endFOV = 17f;
+    [SerializeField] private float _endFocalLength=22;
     [SerializeField] private float _duration = 2f;
+    
 
     private void Start()
     {
-        m_Camera = GetComponent<Camera>();
         transform.DOMove(_endPosition, _duration, false);
-        m_Camera.DOFieldOfView(_endFOV, _duration);    
+        m_Camera.DOFieldOfView(_endFOV, _duration);
+        DOTween.To(x => m_Camera.focalLength = x, m_Camera.focalLength, _endFocalLength, _duration);
     }
+
 }
